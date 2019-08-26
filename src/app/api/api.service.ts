53fda4c24db3d6f6755048c3ebebcd12d5f40285
acvcmaster <acvcnos@hotmail.com>
2019-08-26 20:48:36 -0300

    Gets apiUrl from environment now

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { FileIndexEntry } from './types/file-index-entry';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http'
 import { ApiResponse } from './types/api-response';
+import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  public apiUrl: string = 'http://10.0.0.50:8080/api/File';
   constructor(private httpClient: HttpClient) { }
 
   getFileIndex() : Observable<FileIndexEntry[]>
   {
-    return this.httpClient.get<ApiResponse>(`${this.apiUrl}/GetFileIndex`).pipe(map((data) => data.result));
+    return this.httpClient.get<ApiResponse>(`${environment.apiUrl}/GetFileIndex`).pipe(map((data) => data.result));
   }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FileIndexEntry } from '../api/types/file-index-entry';
 import { ApiService } from '../api/api.service';
 import { ChangeDetectorRef } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tab3',
@@ -12,9 +13,11 @@ export class Tab3Page implements OnInit {
 
   constructor(private apiService: ApiService, private cdr: ChangeDetectorRef) { }
   private fileList: FileIndexEntry[] = [];
+  private environment: any;
 
   ngOnInit(): void {
     this.apiService.getFileIndex().subscribe((values) => this.fileList = values);
+    this.environment = environment;
   }
 
   onRefresh(event: any) { // TODO: implement logic
