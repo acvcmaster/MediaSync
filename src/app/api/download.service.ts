3451151b190083fb6083f08fffe067f72b462b6f
acvcmaster <acvcnos@hotmail.com>
2019-09-21 02:11:39 -0300

    Filesystem now fully works. Download errors appear on the file list view. File transfer is now way faster and memory-efficient.  Local path visible on details when file is downloaded.

--- src/app/api/download.service.ts
+++ src/app/api/download.service.ts
@@ -1,10 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
+import { Subject } from 'rxjs';
 import { File } from '@ionic-native/file/ngx';
-import { HttpClient } from '@angular/common/http'
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer/ngx';
 import { environment } from 'src/environments/environment';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +10,16 @@ import { catchError } from 'rxjs/operators';
 export class DownloadService {
 
   private downloading: string[] = [];
-  private fileSystemMock: string[] = [];
+  private fileSystem: string[] = [];
+  private fileSystemError: string[] = [];
   private downloadQueue: any[] = [];
   public changed: Subject<unknown> = new Subject();
   private storagePath: string;
   private downloadsFolder: string;
   private mediaFolder: string;
+  private downloader: FileTransferObject;
 
-  constructor(private file: File, private httpClient: HttpClient) {
+  constructor(private file: File, private fileTransfer: FileTransfer) {
     this.changed.subscribe(() => {
       if (this.downloadQueue.length && this.downloading.length < 2) {
         const queueDownload = this.downloadQueue.pop();
@@ -29,7 +29,7 @@ export class DownloadService {
     this.storagePath = this.file.externalRootDirectory;
     this.downloadsFolder = `${this.storagePath}Download/`;
     this.mediaFolder = `${this.downloadsFolder}Media/`;
-
+    this.downloader = this.fileTransfer.create();
     this.probeFileSystem();
   }
 
@@ -48,56 +48,49 @@ export class DownloadService {
     }
 
     this.downloading.push(file);
-    const downloadSubscription = this.getFile(file).subscribe((result) => {
-      this.storagePath ? this.downloadFileAndroid(result, file, onFinish, downloadSubscription)
-        : this.downloadFileMocked(result, file, onFinish, downloadSubscription);
+    this.removeError(file);
+
+    this.storagePath ? this.downloadFileAndroid(file, onFinish)
+      : this.downloadFileMocked(file, onFinish);
+  }
+
+  private downloadFileAndroid(file: string, onFinish: () => void) {
+    this.checkDirectoriesAndSave(file, () => {
+      if (onFinish) {
+        onFinish();
+      }
+      this.fileSystem.push(file);
+      this.removeDownload(file);
+      this.changed.next();
+    }, () => {
+      this.fileSystemError.push(file);
+      this.removeDownload(file);
+      this.changed.next();
     });
   }
 
-  private downloadFileAndroid(result: any, file: string, onFinish: () => void, downloadSubscription: Subscription) {
-    if (result instanceof Blob) {
-      this.checkDirectoriesAndSave(file, result, () => {
-        if (onFinish) {
-          onFinish();
-        }
-        this.fileSystemMock.push(file);
-        this.removeDownload(file);
-        this.changed.next();
-        downloadSubscription.unsubscribe();
-      }, (error) => {
-        console.log(error);
-        console.log(`Failed to save file ${file}.`);
-      });
-    }
-  }
-
-  checkDirectoriesAndSave(file: string, result: Blob, callback: () => void, onFail: (error: any) => void) {
+  checkDirectoriesAndSave(file: string, callback: () => void, onFail: () => void) {
     this.file.checkDir(this.downloadsFolder, 'Media').then(_ => {
-      this.file.writeFile(this.mediaFolder, file, result, { replace: true }).then(() => callback()).catch((error) => onFail(error));
+      this.downloader.download(this.getFileUrl(file), `${this.mediaFolder}${file}`).then(() => callback()).catch(() => onFail());
     }).catch(_ => {
       this.file.createDir(this.downloadsFolder, 'Media', true).then(_ => {
-        this.file.writeFile(this.mediaFolder, file, result, { replace: true }).then(() => callback()).catch((error) => onFail(error));
-      }).catch((error) => onFail(error));
+      this.downloader.download(this.getFileUrl(file), `${this.mediaFolder}${file}`).then(() => callback()).catch(() => onFail());
+      }).catch(() => onFail());
     });
   }
 
-  private downloadFileMocked(result: any, file: string, onFinish: () => void, downloadSubscription: Subscription) {
-    if (result instanceof Blob) {
-      this.fileSystemMock.push(file);
-    }
+  private downloadFileMocked(file: string, onFinish: () => void) {
+    this.fileSystem.push(file);
+
     if (onFinish) {
       onFinish();
     }
     this.removeDownload(file);
     this.changed.next();
-    downloadSubscription.unsubscribe();
   }
 
-  getFile(file: string): Observable<any> {
-    return this.httpClient.get<any>(`${environment.apiUrl}/GetFile?file=${file}&raw=true`,
-      { responseType: 'blob' as 'json' }).pipe(catchError(_ => {
-        return of('failed');
-      }));
+  getFileUrl(file: string): string {
+    return `${environment.apiUrl}/GetFile?file=${file}&raw=true`;
   }
 
   removeFile(file: string, onFinish: () => void) {
@@ -111,24 +104,25 @@ export class DownloadService {
   }
 
   removeFileMock(file: string, onFinish: () => void) {
-    const fileIndex: number = this.fileSystemMock.findIndex((elem) => elem === file);
+    const fileIndex: number = this.fileSystem.findIndex((elem) => elem === file);
     if (fileIndex != -1) {
-      this.fileSystemMock.splice(fileIndex, 1);
+      this.fileSystem.splice(fileIndex, 1);
     }
     onFinish();
   }
 
   probeFileSystem() {
+    this.fileSystem = [];
     if (this.storagePath) {
       this.file.listDir(this.downloadsFolder, 'Media').then((entries) => {
-        this.fileSystemMock = entries.map((entry) => entry.name);
+        this.fileSystem = entries.map((entry) => entry.name);
         this.changed.next();
       });
     }
   }
 
   private onFileSystem(file: string): boolean {
-    return this.fileSystemMock.findIndex((elem) => elem === file) !== -1;
+    return this.fileSystem.findIndex((elem) => elem === file) !== -1;
   }
 
   public isDownloading(file: string): boolean {
@@ -139,10 +133,25 @@ export class DownloadService {
     return !this.isDownloading(file) && this.onFileSystem(file);
   }
 
+  public hasError(file: string): boolean {
+    return this.fileSystemError.findIndex((elem) => elem === file) !== -1;
+  }
+
   private removeDownload(file: string) {
     const downloadIndex: number = this.downloading.findIndex((elem) => elem === file);
     if (downloadIndex != -1) {
       this.downloading.splice(downloadIndex, 1);
     }
+  }
+
+  private removeError(file: string) {
+    const errorIndex: number = this.fileSystemError.findIndex((elem) => elem === file);
+    if (errorIndex != -1) {
+      this.fileSystemError.splice(errorIndex, 1);
+    }
+  }
+
+  public getLocalPath(file: string): Promise<string> {
+    return this.file.checkFile(this.mediaFolder, file).then((exists) => exists ? `${this.mediaFolder}${file}` : null).catch(() => null);
   }
 }
